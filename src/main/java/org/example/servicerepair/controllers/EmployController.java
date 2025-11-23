7f5a24d25396d18fa5b076950938aeeac1c63565
Den <dend51108@gmail.com>
2025-11-23 18:09:58 +0300

    add: новые методы для employ

--- src/main/java/org/example/servicerepair/controllers/EmployController.java
+++ src/main/java/org/example/servicerepair/controllers/EmployController.java
@@ -5,9 +5,7 @@ import org.example.servicerepair.source.Employ;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,5 +30,50 @@ public class EmployController {
         model.addAttribute("totalPages", totalPages);
 
         return "employs/employList";
+    }
+
+    @GetMapping("/addEmploy")
+    public String addEmploy(Model model) {
+        model.addAttribute("employ", new Employ());
+        return "employs/addEmploy";
+    }
+
+    @PostMapping("")
+    public String addEmploy(Employ employ) {
+        employDAO.addEmploy(employ);
+        return "redirect:/employs";
+    }
+
+    @GetMapping("/{id}")
+    public String showById(Model model, @PathVariable("id") int id) {
+        Employ employs = employDAO.getById(id);
+        if (employs != null) {
+            model.addAttribute("employs", employs);
+            return "employs/showEmploy";
+        }
+        return "redirect:/employs";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String editEmploy(Model model, @PathVariable("id") int id) {
+        Employ employs = employDAO.getById(id);
+
+        if (employs != null) {
+            model.addAttribute("employ", employs);
+            return "employs/editEmploy";
+        }
+        return "redirect:/employs";
+    }
+
+    @PostMapping("/{id}/update")
+    public String updateEmploy(@ModelAttribute("employ") Employ employ, @PathVariable("id") int id) {
+        employDAO.editEmploy(id, employ);
+        return "redirect:/employs";
+    }
+
+    @PostMapping("{id}/delete")
+    public String deleteEmploy(@PathVariable("id") int id) {
+        employDAO.deleteById(id);
+        return "redirect:/employs";
     }
 }
--- src/main/java/org/example/servicerepair/dao/EmployDAO.java
+++ src/main/java/org/example/servicerepair/dao/EmployDAO.java
@@ -28,4 +28,23 @@ public class EmployDAO {
     public int countEmploys() {
         return jdbcTemplate.queryForObject("SELECT COUNT(*) FROM employ", Integer.class);
     }
+
+    public void addEmploy(Employ employ) {
+        jdbcTemplate.update("INSERT INTO employ(first_name,surname, last_name, position, phone, email) VALUES (?, ?, ?, ?, ?, ?)",
+                employ.getFirst_name(), employ.getSurname(), employ.getLast_name(), employ.getPosition(), employ.getPhone(), employ.getEmail());
+    }
+
+    public void editEmploy(int id, Employ employ) {
+        jdbcTemplate.update("UPDATE employ SET first_name=?, surname=?, last_name=?, position=?, phone=?, email=? WHERE id_employ=?",
+                employ.getFirst_name(), employ.getSurname(), employ.getLast_name(), employ.getPosition(), employ.getPhone(), employ.getEmail(), id);
+    }
+
+    public Employ getById(int id) {
+        return jdbcTemplate.query("SELECT * FROM employ WHERE id_employ = ?", new Object[]{id},
+                new BeanPropertyRowMapper<>(Employ.class)).stream().findAny().orElse(null);
+    }
+
+    public void deleteById(int id) {
+        jdbcTemplate.update("DELETE FROM employ WHERE id_employ = ?", id);
+    }
 }
