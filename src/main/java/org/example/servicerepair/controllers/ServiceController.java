782ac4734a5795f9351fb82081d6146ce43e343c
Den <dend51108@gmail.com>
2025-11-28 15:48:37 +0300

    add: отображение Services

--- src/main/java/org/example/servicerepair/controllers/ServiceController.java
+++ src/main/java/org/example/servicerepair/controllers/ServiceController.java
@@ -1,10 +1,15 @@
 package org.example.servicerepair.controllers;
 
 import org.example.servicerepair.dao.ServicesDAO;
+import org.example.servicerepair.source.Services;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/services")
@@ -14,5 +19,18 @@ public class ServiceController {
     @Autowired
     public ServiceController(ServicesDAO servicesDAO) {
         this.servicesDAO = servicesDAO;
+    }
+
+    @GetMapping("")
+    public String index(Model model, @RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "5") int size) {
+        List<Services> services = servicesDAO.getAllServices(page, size);
+        int totalServices = servicesDAO.countServices();
+        int totalPage = (int) Math.ceil((double) totalServices / size);
+
+        model.addAttribute("servicesList", services);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", totalPage);
+
+        return "services/servicesList";
     }
 }
--- src/main/java/org/example/servicerepair/dao/ServicesDAO.java
+++ src/main/java/org/example/servicerepair/dao/ServicesDAO.java
@@ -1,8 +1,12 @@
 package org.example.servicerepair.dao;
 
+import org.example.servicerepair.source.Services;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class ServicesDAO {
@@ -11,5 +15,16 @@ public class ServicesDAO {
     @Autowired
     public ServicesDAO(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Services> getAllServices(int page, int size) {
+        int offset = (page - 1) * size;
+
+        return jdbcTemplate.query("SELECT * FROM services ORDER BY id_service LIMIT ? OFFSET ?",
+                new BeanPropertyRowMapper<>(Services.class), size, offset);
+    }
+
+    public int countServices() {
+        return jdbcTemplate.queryForObject("SELECT count(*) FROM services", Integer.class);
     }
 }
